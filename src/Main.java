04cd09e71fc7cc5684fdf0b3801a29cfd648acaa
iladanilenko <Danilenkoi@inbox.ru>
2023-01-25 14:38:56 +0300

    HW_4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import transport.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        for (int i = 1; i <= 4; i++){
+
+            DriverB driverB = new DriverB("Driver cat.B № " + i, true, 5 + i);
+            Car car = new Car("Car brand " + i, "Car model " + i, 2, driverB);
+
+            DriverC driverC = new DriverC("Driver cat.C № " + i, true, 7 + i);
+            Trucks trucks = new Trucks ("Trucks brand " + i, "Trucks model " + i, 5, driverC);
+
+            DriverD driverD = new DriverD("Driver cat.D № " + i, true, 10 + i);
+            Bus bus = new Bus ("Bus brand " + i, "Bus model " + i, 4, driverD);
+
+            printInfo(car);
+            printInfo(trucks);
+            printInfo(bus);
+        }
+    }
+    private static void printInfo (Transport<?> transport){
+        System.out.println("водитель " + transport.getDriver().getName() + " управляет автомобилем " + transport.getBrand()
+                + " и будет участвовать в заезде");
     }
 }
